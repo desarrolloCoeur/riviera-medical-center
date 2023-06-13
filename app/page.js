d9ccb9852bf901d31e6da1757a17f7813d2efbe7
CoeurMkt <desarrollo@coeurmkt.com>
2023-06-13 09:25:46 -0600

    change import prefix

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-import Services from '@/components/home/Services'
-import Team from '@/components/home/Team'
-import Hero from '@/components/ui/Hero'
+import Services from '../components/home/Services'
+import Team from '../components/home/Team'
+import Hero from '../components/ui/Hero'
 import Image from 'next/image'
 
 export default function Home() {
