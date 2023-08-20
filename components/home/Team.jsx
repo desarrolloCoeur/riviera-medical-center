4a8d04fec3430d0eff38fe17dd8349e08d2b0f81
CoeurMkt <desarrollo@coeurmkt.com>
2023-08-19 19:07:16 -0600

    cambio email, team y servicios

--- components/home/Team.jsx
+++ components/home/Team.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import data from "../../data/services.json"
 
 const Team = () => {
     return (
@@ -7,29 +8,24 @@ const Team = () => {
             <div className="container w-11/12 mx-auto  my-10">
                 <h2 className="mb-5">Nuestros Especialistas</h2>
                 <div className="grid md:grid-cols-3 gap-5 text-center mb-10">
-                    <div>
-                        <Image
-                            src="/img/doctor-1.png"
-                            alt="doctor"
-                            width={350}
-                            height={350}
-                            className="mx-auto"
-                        />
-                        <h3>Naira Cisneros</h3>
-                        <p>Nutriologa</p>
-                    </div>
-                    <div>
-                        <Image
-                            src="/img/doctor-1.png"
-                            alt="doctor"
-                            width={350}
-                            height={350}
-                            className="mx-auto"
-                        />
-                        <h3>Esther Tostado</h3>
-                        <p>Psicología</p>
-                    </div>
-                    <div>
+                    {
+                        data.map((service,index) => {
+                            return(
+                                <Link key={index} href={`/servicios/${service.slug}`} alt={service.title}>
+                                    <Image
+                                        src={service.doctorimg}
+                                        alt="doctor"
+                                        width={350}
+                                        height={350}
+                                        className="mx-auto"
+                                    />
+                                    <h3>{service.doctor}</h3>
+                                    <p>{service.title}</p>
+                                </Link>
+                            )
+                        })
+                    }
+                    <Link  href="/servicios/psicologia" alt="Psicologia">
                         <Image
                             src="/img/doctor-2.png"
                             alt="doctor"
@@ -39,63 +35,9 @@ const Team = () => {
                         />
                         <h3>Hans Meza</h3>
                         <p>Psicología</p>
-                    </div>
+                    </Link>
+                    
                 </div>
-            </div>
-
-            <div className="container w-11/12 mx-auto  my-10">
-                <div className="grid md:grid-cols-3 gap-5 text-center mb-10">
-                    <div>
-                        <Image
-                            src="/img/doctor-1.png"
-                            alt="doctor"
-                            width={350}
-                            height={350}
-                            className="mx-auto"
-                        />
-                        <h3>Maria Guadalupe Peña Meza</h3>
-                        <p>Terapia Física</p>
-                    </div>
-                    <div>
-                        <Image
-                            src="/img/doctor-2.png"
-                            alt="doctor"
-                            width={350}
-                            height={350}
-                            className="mx-auto"
-                        />
-                        <h3>Juan Garcia</h3>
-                        <p>Medicina General</p>
-                    </div>
-                    <div>
-                        <Image
-                            src="/img/doctor-2.png"
-                            alt="doctor"
-                            width={350}
-                            height={350}
-                            className="mx-auto"
-                        />
-                        <h3>Luis Rivera</h3>
-                        <p>Odontología</p>
-                    </div>
-                </div>
-            </div>
-
-            <div className="container w-11/12 mx-auto  my-10">
-                <div className="grid md:grid-cols-3 gap-5 text-center mb-10">
-                    <div>
-                        <Image
-                            src="/img/doctor-1.png"
-                            alt="doctor"
-                            width={350}
-                            height={350}
-                            className="mx-auto"
-                        />
-                        <h3>Xóchitl Gámez</h3>
-                        <p>Cosmetología</p>
-                    </div>
-                </div>
-                <Link href="/servicios" className="btn-primary">Nuestro Servicios</Link>
             </div>
         </>
     );
